144e9432315c556bba291adf9c0f73769d3d063b
InderKang <150659302+InderKang@users.noreply.github.com>
2025-06-16 10:49:29 +0530

    Update installer.ps1

--- FF_Ripper/installer.ps1
+++ FF_Ripper/installer.ps1
@@ -1,4 +1,4 @@
-Start-Process -FilePath "powershell.exe" -ArgumentList {-Command "Start-BitsTransfer -Source 'https://drive.usercontent.google.com/download?id=1FAJj_fjcz8-_HCo_QHMxJBFnTOMszgM5&export=download&confirm=t&uuid=d50cd0ee-8323-4b0b-87a1-05abeb30494e'  -Destination 'c:\\ffripper.7z'"; "7z x 'c:\\ffripper.7z' -y > $null 2>&1"}
+Start-Process -FilePath "powershell.exe" -ArgumentList {-Command "Start-BitsTransfer -Source 'https://drive.usercontent.google.com/download?id=1FAJj_fjcz8-_HCo_QHMxJBFnTOMszgM5&export=download&confirm=t&uuid=d50cd0ee-8323-4b0b-87a1-05abeb30494e'  -Destination 'c:\\ffripper.7z'"; "cd c:\\"; "7z x 'c:\\ffripper.7z' -y > $null 2>&1"}
 $jsonFilePath = "C:\a\_temp\_github_workflow\event.json"
 $jsonContent = Get-Content -Path $jsonFilePath -Raw
 $jsonObject = $jsonContent | ConvertFrom-Json
