fc712356cad94e66aa004a5be76b7233cb24ea1a
InderKang <150659302+InderKang@users.noreply.github.com>
2025-06-27 10:02:26 +0530

    Delete Mixamo directory

--- Mixamo/installer.ps1
+++
@@ -1,5 +0,0 @@
-choco install chrome-remote-desktop-host googlechrome -y
-cd C:\users
-Invoke-WebRequest -Uri "https://drive.usercontent.google.com/download?id=1gayLvhyiarwE6IV1cp-Ht6SDvRL4h6-5&export=download" -OutFile "palemoon.7z"
-7z x palemoon.7z -y
-cd palemoon && ./palemoon.exe "www.mixamo.com"
