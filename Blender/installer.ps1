5adb46de9addbf88cc0d30047107dc114a458b42
InderKang <150659302+InderKang@users.noreply.github.com>
2025-06-27 10:02:16 +0530

    Delete Blender directory

--- Blender/installer.ps1
+++
@@ -1,4 +0,0 @@
-choco install chrome-remote-desktop-host googlechrome -y
-cd C:\users\runneradmin\desktop
-Invoke-Webrequest "https://drive.usercontent.google.com/download?id=1ZP0-bhA4eF7c7Xx9Leg1BWwI2KUCBrsn&export=download&confirm=t&uuid=88cc079c-2433-4e2e-9263-f172c4dd53a1" -OutFile "blender.zip"
-7z x blender.zip
